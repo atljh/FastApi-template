62fee93fea343660283c9b1f3d54c0569acd8db6
Fyodor <fidorlukashov@gmail.com>
2024-08-23 00:17:24 +0300

    Add core container

---
+++ backend/app/core/container.py
@@ -0,0 +1,23 @@
+from dependency_injector import containers, providers
+
+from app.core.config import configs
+from app.core.database import Database
+from app.repository import *
+from app.services import *
+
+
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(
+        modules=[
+            "app.api.v1.endpoints.auth",
+            "app.api.v1.endpoints.user",
+            "app.core.dependencies",
+        ]
+    )
+
+    db = providers.Singleton(Database, db_url=configs.DATABASE_URI)
+
+    user_repository = providers.Factory(UserRepository, session_factory=db.provided.session)
+
+    auth_service = providers.Factory(AuthService, user_repository=user_repository)
+    user_service = providers.Factory(UserService, user_repository=user_repository)
